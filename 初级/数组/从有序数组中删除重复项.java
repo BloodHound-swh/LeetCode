5901bfb166e7c4c3db5f6609d95dbb500b8715c2
BloodHound-swh <bloodhoundswh@gmail.com>
2018-04-09 15:55:48 +0800

    第一题
    初次尝试

---
+++ 初级/数组/从有序数组中删除重复项.java
@@ -0,0 +1,40 @@
+/*
+从排序数组中删除重复项
+给定一个有序数组，你需要原地删除其中的重复内容，使每个元素只出现一次,并返回新的长度。
+
+不要另外定义一个数组，您必须通过用 O(1) 额外内存原地修改输入的数组来做到这一点。
+
+示例：
+
+给定数组: nums = [1,1,2],
+
+你的函数应该返回新长度 2, 并且原数组nums的前两个元素必须是1和2
+不需要理会新的数组长度后面的元素
+*/
+
+
+//笔记：使用快慢指针来记录遍历的坐标，最开始时两个指针都指向第一个数字，如果两个指针指的数字相同，则快指针向前走一步。
+//     如果不同，则两个指针都向前走一步，这样当快指针走完整个数组后，慢指针当前的坐标加1就是数组中不同数字的个数
+
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums == null) {
+            return 0;
+        }
+        if (nums.length <= 1) {
+            return nums.length;
+        }
+        
+        int currpos = 0;
+        int movepos = 1;
+        
+        for(movepos = 1; movepos<nums.length; movepos++){
+            if(nums[currpos]!=nums[movepos]){
+                nums[++currpos]=nums[movepos];
+            }
+        }
+        
+        return currpos+1;
+    }
+}
