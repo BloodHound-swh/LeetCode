eb79f48e7219664b81ede330a2a1e8189a93c8cd
BloodHound-swh <bloodhoundswh@gmail.com>
2018-05-07 10:53:54 +0800

    合并两个有序链表

---
+++ 初级/链表/04-合并两个有序链表.java
@@ -0,0 +1,104 @@
+/**
+合并两个有序链表
+将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+
+示例：
+
+输入：1->2->4, 1->3->4
+输出：1->1->2->3->4->4
+ */
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+
+//使用递归
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null)
+            return l2;
+        if(l2 == null)
+            return l1;
+        
+        ListNode mergeHead = null;
+        if(l1.val<=l2.val){
+            mergeHead = l1;
+            mergeHead.next = mergeTwoLists(l1.next, l2);
+        } else {
+            mergeHead = l2;
+            mergeHead.next = mergeTwoLists(l1, l2.next);
+        }
+        
+        return mergeHead;
+    }
+}
+
+//使用指针p每次指向小的那个节点，最后把没有比完的list拼接在末尾即可。
+class Solution{
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2){
+        if(l1 == null)
+            return l2;
+        if(l2 == null)
+            return l1;
+
+        ListNode p = new ListNode(0);
+        ListNode head = p;
+
+        while(l1!=null && l2!=null){
+            if(l1.val<=l2.val){
+                p.next = l1;
+                l1 = l1.next;
+            }else{
+                p.next = l2;
+                l2 = l2.next;
+            }
+            p = p.next;
+        }
+
+        if(l1!=null){
+            p.next = l1;
+        }else if(l2 != null){
+            p.next = l2;
+        }
+        return head.next;
+
+    }
+}
+
+//对上面迭代方法的代码进行优化
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) {
+            return null;
+        }
+        ListNode node = new ListNode(0);
+        ListNode dummy = node;
+        while (l1 != null || l2 != null) {
+            if (l1 == null) {
+                node.next = l2;
+                break;
+            } else if (l2 == null) {
+                node.next = l1;
+                break;
+            } else {
+                if (l1.val < l2.val) {
+                    node.next = l1;
+                    l1 = l1.next;
+                } else {
+                    node.next = l2;
+                    l2 = l2.next;
+                }
+                node = node.next;
+            }
+        }//end while
+        return dummy.next;
+    }
+}
