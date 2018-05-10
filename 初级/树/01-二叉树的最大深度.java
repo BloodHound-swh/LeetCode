149cd96467266eed097c41429a6255ba9078178c
BloodHound-swh <bloodhoundswh@gmail.com>
2018-05-10 10:40:24 +0800

    二叉树的最大深度

---
+++ 初级/树/01-二叉树的最大深度.java
@@ -0,0 +1,59 @@
+/**
+二叉树的最大深度
+给定一个二叉树，找出其最大深度。
+
+二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+
+说明: 叶子节点是指没有子节点的节点。
+
+示例：
+给定二叉树 [3,9,20,null,null,15,7]，
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回它的最大深度 3 。
+ */
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+ //递归思想——找到每一层所重复处理的方法，再找到边界条件，就可以确定递归。
+
+
+//树的最大深度等于左子树和右子树最大深度的最大值加1，以此递归即可，注意边界条件。
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null)
+            return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
+
+//常用写法
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: An integer.
+     */
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        
+        return Math.max(left, right) + 1;
+    }
+}
