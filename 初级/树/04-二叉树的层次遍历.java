3078a63a3ada7e802692327e8af014bae544acd2
BloodHound-swh <bloodhoundswh@gmail.com>
2018-05-11 16:42:10 +0800

    二叉树的层次遍历

---
+++ 初级/树/04-二叉树的层次遍历.java
@@ -0,0 +1,59 @@
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+
+ //使用一个队列queue,按顺序装入每层的节点，queue.size()记录每层节点的个数。然后把每层的节点值add进list中，最后把一个个list放入打最终的list of list
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null)
+            return result;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for(int i=0;i<size;i++){    // 这里的size每次都是上一层节点的个数
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if(node.left!=null)
+                    queue.offer(node.left);
+                if(node.right!=null)
+                    queue.offer(node.right);
+            }
+            result.add(list);
+        }
+        return result;
+    }
+}
+
+//使用类似前序遍历的方法，递归得到
+//每次进入一层level，result添加一个ArrayList（因为需要顺序）。List存储该层的节点值。然后按顺序分别进入左右子节点，每次往下走一层时，level+1。
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null)
+            return result;
+        helper(result, 0, root);
+        return result;
+    }
+    public static void helper(List<List<Integer>> result, int level, TreeNode root){
+        if(root == null)
+            return;
+        if(level >= result.size())//或者 直接用==
+            result.add(new ArrayList<Integer>());
+        result.get(level).add(root.val);
+        helper(result, level + 1, root.left);
+        helper(result, level + 1, root.right);
+    }
+}
