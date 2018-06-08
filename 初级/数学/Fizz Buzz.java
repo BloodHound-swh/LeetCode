5c9b11ea999592469981b05945a6644a445bc316
BloodHound-swh <bloodhoundswh@gmail.com>
2018-06-08 09:42:13 +0800

    FizzBuzz

---
+++ 初级/数学/Fizz Buzz.java
@@ -0,0 +1,54 @@
+/**
+Fizz Buzz
+Write a program that outputs the string representation of numbers from 1 to n.
+
+But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
+
+Example:
+
+n = 15,
+
+Return:
+[
+    "1",
+    "2",
+    "Fizz",
+    "4",
+    "Buzz",
+    "Fizz",
+    "7",
+    "8",
+    "Fizz",
+    "Buzz",
+    "11",
+    "Fizz",
+    "13",
+    "14",
+    "FizzBuzz"
+]
+给你一个整数n. 从 1 到 n 按照下面的规则打印每个数：
+
+如果这个数被3整除，打印fizz.
+如果这个数被5整除，打印buzz.
+如果这个数能同时被3和5整除，打印fizz buzz.
+ */
+
+
+//就是简单的条件判定，注意i+""是将i转变成字符串，也可以用String.valueOf(i)
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> res = new ArrayList<String>();
+        for(int i = 1; i <= n; i++){
+            if(i % 3 ==0 && i % 5 == 0){
+                res.add("FizzBuzz");
+            } else if(i % 3 == 0){
+                res.add("Fizz");
+            } else if(i % 5 == 0){
+                res.add("Buzz");
+            } else{
+                res.add(i+"");
+            }
+        }
+        return res;
+    }
+}
