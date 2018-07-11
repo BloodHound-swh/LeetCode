d169307a13b8694506a9bbc312911ac96a6bff6e
BloodHound-swh <bloodhoundswh@gmail.com>
2018-07-11 10:51:31 +0800

    中序遍历二叉树

--- 中级/树和图/01-中序遍历二叉树.java
+++ 中级/树和图/01-中序遍历二叉树.java
@@ -44,6 +44,11 @@ class Solution {
 }
 
 //迭代
+// 使用一个栈来存储二叉树节点，根据中序遍历的规则，我们可以推算出这样的规律： 
+// 1. 将当前非空节点入栈 
+// 2. 如果左子节点不为空，则继续将左子节点入栈 
+// 3. 如果左子节点为空，则抛出栈顶节点并记录 val 值，然后将其右子节点入栈 
+// 4. 重复 1、2、3 步骤直至栈空
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
