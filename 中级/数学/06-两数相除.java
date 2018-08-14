f4bcef2275adca23004a8d249ca59b9257190b5e
BloodHound-swh <bloodhoundswh@gmail.com>
2018-08-14 11:28:13 +0800

    两数相除

---
+++ 中级/数学/06-两数相除.java
@@ -0,0 +1,88 @@
+/**
+ * 两数相除
+给定两个整数，被除数 dividend 和除数 divisor。将两数相除，要求不使用乘法、除法和 mod 运算符。
+
+返回被除数 dividend 除以除数 divisor 得到的商。
+
+示例 1:
+
+输入: dividend = 10, divisor = 3
+输出: 3
+示例 2:
+
+输入: dividend = 7, divisor = -3
+输出: -2
+说明:
+
+被除数和除数均为 32 位有符号整数。
+除数不为 0。
+假设我们的环境只能存储 32 位有符号整数，其数值范围是 [−2^31,  2^31 − 1]。本题中，如果除法结果溢出，则返回 2^31 − 1。
+ */
+
+
+
+// 让除数乘2，逼近被除数知道超过，然后被除数减去之前的最大值，所得的差值如果依然大于除数，让除数重新乘2继续逼近
+class Solution {
+    public int divide(int dividend, int divisor) {
+        if (divisor == 0)
+            return Integer.MAX_VALUE;
+        if (dividend == Integer.MIN_VALUE) {
+            if (divisor == -1)
+                return Integer.MAX_VALUE;
+            else if (divisor == 1)
+                return Integer.MIN_VALUE;
+        }
+        long divd = (long) dividend;
+        long divs = (long) divisor;
+        int sign = 1;
+        if (divd < 0) {
+            divd = -divd;
+            sign = -sign;
+        }
+        if (divs < 0) {
+            divs = -divs;
+            sign = -sign;
+        }
+        int res = 0;
+        while (divd >= divs) {
+            int shift = 0;
+            while (divd >= divs << shift) {
+                shift++;
+            }
+            res += (1 << (shift - 1));
+            divd -= (divs << (shift - 1));
+        }
+        return sign * res;
+    }
+}
+
+// 简化了符号判定部分的代码
+class Solution {
+    public int divide(int dividend, int divisor) {
+        if (divisor == 0)
+            return Integer.MAX_VALUE;
+        if (dividend == Integer.MIN_VALUE) {
+            if (divisor == -1)
+                return Integer.MAX_VALUE;
+            else if (divisor == 1)
+                return Integer.MIN_VALUE;
+        }
+
+        int sign = ((dividend < 0) ^ (divisor < 0)) ? -1 : 1;
+        long dvd = Math.abs((long) dividend);
+        long dvs = Math.abs((long) divisor);
+
+        int res = 0;
+        while (dvs <= dvd) {
+            long tmp = dvs;
+            long mul = 1;
+            while (dvd >= (tmp << 1)) {
+                tmp <<= 1;
+                mul <<= 1;
+            }
+            dvd -= tmp;
+            res += mul;
+        }
+        return res * sign;
+    }
+}
