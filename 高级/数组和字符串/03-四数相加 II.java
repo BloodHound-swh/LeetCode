2f5946d8e14199beaa418bfefabb66cf0b3241ae
BloodHound-swh <31639047+BloodHound-swh@users.noreply.github.com>
2018-08-17 21:44:49 +0800

    四数相加 II

---
+++ 高级/数组和字符串/03-四数相加 II.java
@@ -0,0 +1,46 @@
+/**
+ *  四数相加 II
+给定四个包含整数的数组列表 A , B , C , D ,计算有多少个元组 (i, j, k, l) ，使得 A[i] + B[j] + C[k] + D[l] = 0。
+
+为了使问题简单化，所有的 A, B, C, D 具有相同的长度 N，且 0 ≤ N ≤ 500 。所有整数的范围在 -228 到 228 - 1 之间，最终结果不会超过 231 - 1 。
+
+例如:
+
+输入:
+A = [ 1, 2]
+B = [-2,-1]
+C = [-1, 2]
+D = [ 0, 2]
+
+输出:
+2
+
+解释:
+两个元组如下:
+1. (0, 0, 0, 1) -> A[0] + B[0] + C[0] + D[1] = 1 + (-2) + (-1) + 2 = 0
+2. (1, 1, 0, 0) -> A[1] + B[1] + C[0] + D[0] = 2 + (-1) + (-1) + 0 = 0
+ */
+
+
+// 我们如果把A和B的两两之和都求出来，在哈希表中建立两数之和跟其出现次数之间的映射
+// 那么我们再遍历C和D中任意两个数之和，我们只要看哈希表存不存在这两数之和的相反数就行了
+class Solution {
+    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < C.length; i++) {
+            for (int j = 0; j < D.length; j++) {
+                int sum = C[i] + D[j];
+                map.put(sum, map.getOrDefault(sum, 0) + 1); // 如果key对应的键有值，那么返回这个值；否则返回这个指定的默认值defaultValue
+            }
+        }
+
+        int res = 0;
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < B.length; j++) {
+                res += map.getOrDefault(-1 * (A[i] + B[j]), 0);
+            }
+        }
+
+        return res;
+    }
+}
