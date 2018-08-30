79856fb5f50bf81fbddab19cb6009155b298f0ae
BloodHound-swh <31639047+BloodHound-swh@users.noreply.github.com>
2018-08-30 19:03:02 +0800

    链表排序

---
+++ 高级/链表/02-链表排序.java
@@ -0,0 +1,92 @@
+/**
+ * 链表排序
+在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序。
+
+示例 1:
+
+输入: 4->2->1->3
+输出: 1->2->3->4
+示例 2:
+
+输入: -1->5->3->4->0
+输出: -1->0->3->4->5
+ */
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+// 使用的是归并排序
+// 使用递归，将链表分成一个个的节点，然后自下而上的merge
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode middle = slow.next;
+        slow.next = null;
+        return merge(sortList(head), sortList(middle)); // 递归
+    }
+
+    public ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        if (l1 != null)
+            cur.next = l1;
+        if (l2 != null)
+            cur.next = l2;
+        return dummy.next;
+    }
+}
+
+// 这种方法也是归并排序，而且在merge函数中也使用了递归，这样使代码更加简洁啦～
+public class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode slow = head, fast = head, pre = head;
+        while (fast != null && fast.next != null) {
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        pre.next = null;
+        return merge(sortList(head), sortList(slow));
+    }
+
+    public ListNode merge(ListNode l1, ListNode l2) {
+        if (l1 == null)
+            return l2;
+        if (l2 == null)
+            return l1;
+        if (l1.val < l2.val) {
+            l1.next = merge(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = merge(l1, l2.next);
+            return l2;
+        }
+    }
+}
