572d08e16d2be6cb37b352d966752bace6a0372d
BloodHound-swh <bloodhoundswh@gmail.com>
2018-09-04 23:12:17 +0800

    计算右侧小于当前元素的个数

--- 高级/树和图/09-计算右侧小于当前元素的个数.java
+++ 高级/树和图/09-计算右侧小于当前元素的个数.java
@@ -118,3 +118,39 @@ class Solution {
         }
     }
 }
+
+// 二分法
+class Solution {
+    public List<Integer> countSmaller(int[] nums) {
+        Integer[] res = new Integer[nums.length];
+        List<Integer> list = new ArrayList<>();
+        for (int i = nums.length - 1; i >= 0; i--) {
+            int index = findIndex(list, nums[i]);
+            res[i] = index;
+            list.add(index, nums[i]);
+        }
+        return Arrays.asList(res);
+    }
+
+    public int findIndex(List<Integer> list, int target) {
+        if (list.size() == 0) 
+            return 0;
+        int start = 0;
+        int end = list,size() - 1;
+        if (list.get(end) < target) 
+            return end + 1;
+        if (list.get(start) >= target) 
+            return 0;
+        while (start + 1 < end) {
+            int mid = (end - start) / 2 + start;
+            if (list.get(mid) < target) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        if (list.get(start) >= target) 
+            return start;
+        return end;
+    }
+}
