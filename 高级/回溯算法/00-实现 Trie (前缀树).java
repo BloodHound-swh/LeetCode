acf0e38bc9de974f9c2713069f786a3089ca78bd
BloodHound-swh <bloodhoundswh@gmail.com>
2018-09-05 15:34:29 +0800

    实现 Trie (前缀树)

--- 高级/回溯算法/00-实现 Trie (前缀树).java
+++ 高级/回溯算法/00-实现 Trie (前缀树).java
@@ -84,3 +84,65 @@ class Trie {
  * Trie(); obj.insert(word); boolean param_2 = obj.search(word); boolean param_3
  * = obj.startsWith(prefix);
  */
+
+// 使用HashMap，思想同方法一
+class Trie {
+
+    private class TrieNode {
+        private HashMap<Character, TrieNode> children;
+        private boolean hasWord;
+
+        public TrieNode() {
+            this.children = new HashMap<>();
+            this.hasWord = false;
+        }
+    }
+
+    private TrieNode root;
+
+    /** Initialize your data structure here. */
+    public Trie() {
+        this.root = new TrieNode();
+    }
+
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        TrieNode curr = this.root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (!curr.children.containsKey(c)) {
+                curr.children.put(c, new TrieNode());
+            }
+            curr = curr.children.get(c);
+        }
+        curr.hasWord = true;
+    }
+
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        TrieNode curr = this.root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (!curr.children.containsKey(c)) {
+                return false;
+            }
+            curr = curr.children.get(c);
+        }
+        return curr.hasWord;
+    }
+
+    /**
+     * Returns if there is any word in the trie that starts with the given prefix.
+     */
+    public boolean startsWith(String prefix) {
+        TrieNode curr = this.root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            if (!curr.children.containsKey(c)) {
+                return false;
+            }
+            curr = curr.children.get(c);
+        }
+        return true;
+    }
+}
