60ed86b72305d5d6cba24ff05994699c3b622406
BloodHound-swh <31639047+BloodHound-swh@users.noreply.github.com>
2018-09-10 21:47:50 +0800

    完全平方数

--- 高级/动态规划/03-完全平方数.java
+++ 高级/动态规划/03-完全平方数.java
@@ -16,6 +16,7 @@
 
 // 如果一个数x可以表示为一个任意数a加上一个平方数bｘb，也就是x=a+bｘb
 // 那么能组成这个数x最少的平方数个数，就是能组成a最少的平方数个数加上1（因为b*b已经是平方数了）。
+// https://blog.csdn.net/happyaaaaaaaaaaa/article/details/51584790
 class Solution {
     public int numSquares(int n) {
         int[] dp = new int[n + 1];
