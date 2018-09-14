3e67eab988119c7efadaf2c6349d9f6c14737d70
BloodHound-swh <bloodhoundswh@gmail.com>
2018-09-14 09:31:22 +0800

    Queue Reconstruction by Height

--- 高级/其他/01-Queue Reconstruction by Height.java
+++ 高级/其他/01-Queue Reconstruction by Height.java
@@ -1,4 +1,23 @@
-public class Solution {
+/**
+ * Queue Reconstruction by Height
+假设有打乱顺序的一群人站成一个队列。 每个人由一个整数对(h, k)表示，其中h是这个人的身高，k是排在这个人前面且身高大于或等于h的人数。 编写一个算法来重建这个队列。
+
+注意：
+总人数少于1100人。
+
+示例
+
+输入:
+[[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+
+输出:
+[[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
+ */
+
+// 首先我们给队列先排个序，按照身高高的排前面，如果身高相同，则第二个数小的排前面。
+// 然后我们新建一个空的数组，遍历之前排好序的数组，然后根据每个元素的第二个数字，将其插入到res数组中对应的位置
+// 这是因为我们先排了身高大的人，之后排身高小的人是不影响身高大的人的k值的
+class Solution {
     public int[][] reconstructQueue(int[][] people) {
         if (people == null || people.length == 0 || people[0].length == 0) {
             return people;
@@ -14,17 +33,41 @@ public class Solution {
             }
         });
 
-        List<int[]> list = new ArrayList<int[]>();
+        List<int[]> list = new ArrayList<>();
         for (int i = 0; i < people.length; i++) {
-            list.add(people[i][1], people[i]);
+            list.add(people[i][1], people[i]); // java.util.ArrayList.add(int index, E elemen) 方法将指定的元素E在此列表中的指定位置。
         }
 
-        int[][] ret = new int[people.length][people[0].length];
-        for (int i = 0; i < ret.length; i++) {
-            ret[i][0] = list.get(i)[0];
-            ret[i][1] = list.get(i)[1];
+        int[][] res = new int[people.length][people[0].length];
+        for (int i = 0; i < res.length; i++) {
+            res[i][0] = list.get(i)[0];
+            res[i][1] = list.get(i)[1];
         }
 
-        return ret;
+        return res;
+    }
+}
+
+// 优化，同方法一
+class Solution {
+    public int[][] reconstructQueue(int[][] people) {
+        if (people == null || people.length == 0 || people[0].length == 0) {
+            return new int[0][0];
+        }
+        Arrays.sort(people, new Comparator<int[]>() {
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] == o2[0]) {
+                    return o1[1] - o2[1];
+                } else {
+                    return o2[0] - o1[0];
+                }
+            }
+        });
+        List<int[]> queue = new ArrayList<>();
+        for (int[] p : people) {
+            queue.add(p[1], p);
+        }
+        return queue.toArray(new int[queue.size()][]);
+
     }
 }
