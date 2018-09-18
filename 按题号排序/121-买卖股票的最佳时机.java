7265a6c3d3d498e1ed31bca4f6586921f5a89412
BloodHound-swh <bloodhoundswh@gmail.com>
2018-09-18 08:56:24 +0800

    Best Time to Buy and Sell Stock

---
+++ 按题号排序/121-买卖股票的最佳时机.java
@@ -0,0 +1,57 @@
+/**
+ * 买卖股票的最佳时机
+给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+
+如果你最多只允许完成一笔交易（即买入和卖出一支股票），设计一个算法来计算你所能获取的最大利润。
+
+注意你不能在买入股票前卖出股票。
+
+示例 1:
+
+输入: [7,1,5,3,6,4]
+输出: 5
+解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+     注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格。
+示例 2:
+
+输入: [7,6,4,3,1]
+输出: 0
+解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+ */
+
+
+
+// 未看答案版
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length <= 1)
+            return 0;
+        int min = prices[0];
+        int max = 0;
+        int profit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] > min) {
+                max = prices[i];
+                int currProfit = max - min;
+                profit = Math.max(currProfit, profit);
+            } else {
+                min = prices[i];
+            }
+        }
+        return profit;
+    }
+}
+
+// 答案，与上面相比，简洁一些
+// 只需要遍历一次数组，用一个变量记录遍历过数中的最小值，然后每次计算当前值和这个最小值之间的差值最为利润，然后每次选较大的利润来更新。
+// 当遍历完成后当前利润即为所求
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int res = 0, buy = Integer.MAX_VALUE;
+        for (int price : prices) {
+            buy = Math.min(buy, price);
+            res = Math.max(res, price - buy);
+        }
+        return res;
+    }
+}
--- 经典题/初级/数组/02-买卖股票最佳时机II.java
+++ 经典题/初级/数组/02-买卖股票最佳时机II.java
@@ -4,22 +4,19 @@
 设计一个算法来找到最大的利润。你可以完成尽可能多的交易（多次买卖股票）。然而，你不能同时参与多个交易（你必须在再次购买前出售股票）。
 */
 
-
-
-
 //这个问题可以被看作找到数组中所有的上升序列问题。例如，对于给定的5，1，2，3，4
 // 从1买进然后从4卖出，和从1买进，2卖出，2买进，3卖出效果是一样的.
 // 所以我们遍历一遍数组，找到所有上升的的序列
 
 class Solution {
     public int maxProfit(int[] prices) {
-        if(prices==null || prices.length==0){
+        if (prices == null || prices.length == 0) {
             return 0;
         }
         int profit = 0;
-        for(int i = 0; i<prices.length-1;i++){
-            if(prices[i+1]-prices[i]>0)
-                profit = profit + prices[i+1]-prices[i];
+        for (int i = 0; i < prices.length - 1; i++) {
+            if (prices[i + 1] - prices[i] > 0)
+                profit = profit + prices[i + 1] - prices[i];
         }
         return profit;
     }
