080a27fe529ea423fd6263003504689c251fe71f
BloodHound-swh <bloodhoundswh@gmail.com>
2018-09-19 08:35:00 +0800

    Contains Duplicate

--- 按题号排序/217-存在重复元素.java
+++ 按题号排序/217-存在重复元素.java
@@ -1,35 +1,43 @@
-/*
-存在重复
+/**
+ * 存在重复元素
+
 给定一个整数数组，判断是否存在重复元素。
 
-如果任何值在数组中出现至少两次，函数应该返回 true。如果每个元素都不相同，则返回 false。
-*/
+如果任何值在数组中出现至少两次，函数返回 true。如果数组中每个元素都不相同，则返回 false。
 
-//
+示例 1:
 
-// 好像是超时了
+输入: [1,2,3,1]
+输出: true
+示例 2:
+
+输入: [1,2,3,4]
+输出: false
+示例 3:
+
+输入: [1,1,1,3,3,4,3,2,4,2]
+输出: true
+ */
+
+
+// 未看答案版
 class Solution {
-    public static boolean containsDuplcate(int[] nums) {
-        if (nums == null || nums.length <= 1) {
+    public boolean containsDuplicate(int[] nums) {
+        if (nums == null || nums.length <= 1)
             return false;
-        }
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] == nums[j]) {
-                    return true;
-                }
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            } else {
+                set.add(nums[i]);
             }
         }
         return false;
     }
-
-    public static void main(String[] args) {
-        int[] nums = { 1, 2, 3, 3 };
-        boolean x = containsDuplcate(nums);
-        System.out.println(x);
-    }
 }
 
+// 答案一
 // 采用一个Set容器即可轻松解决。
 class Solution {
     public boolean containsDuplicate(int[] nums) {
@@ -47,6 +55,7 @@ class Solution {
     }
 }
 
+// 答案二
 // 使用Sorry array, nLog(n)
 class Solution {
     public boolean containsDuplicate(int[] nums) {
