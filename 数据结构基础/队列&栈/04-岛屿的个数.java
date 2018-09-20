7cc6a58b38ea8f4502083870be04ec4e9c51f3a1
BloodHound-swh <bloodhoundswh@gmail.com>
2018-09-20 21:02:06 +0800

    岛屿的个数

---
+++ 数据结构基础/队列&栈/04-岛屿的个数.java
@@ -0,0 +1,92 @@
+/**
+ * 岛屿的个数
+给定一个由 '1'（陆地）和 '0'（水）组成的的二维网格，计算岛屿的数量。一个岛被水包围，并且它是通过水平方向或垂直方向上相邻的陆地连接而成的。你可以假设网格的四个边均被水包围。
+
+示例 1:
+
+输入:
+11110
+11010
+11000
+00000
+
+输出: 1
+示例 2:
+
+输入:
+11000
+11000
+00100
+00011
+
+输出: 3
+ * 
+ */
+
+
+
+// 使用深度优先遍历，探寻每个‘1’的上下左右，如果为1，则置零，如果都不为1，则形成了一个岛屿，res++
+class Solution {
+    private int m;
+    private int n;
+
+    public int numIslands(char[][] grid) {
+        int res = 0;
+        m = grid.length;
+        if (m == 0)
+            return 0;
+        n = grid[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == '0')
+            return;
+        grid[i][j] = '0';
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+    }
+}
+
+// 对dfs的美化，没什么区别，就是简洁一些
+class Solution {
+    private int m, n;
+    private int[][] direction = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    public int numIslands(char[][] grid) {
+        if(grid == null || grid.length == 0){
+            return 0;
+        }
+        m = grid.length;
+        n = grid[0].length;
+        int isLandNum =  0;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] != '0'){
+                    dfs(grid, i, j);
+                    isLandNum++;
+                }
+            }
+        }
+        return isLandNum;
+    }
+    
+    public void dfs(char[][] grid, int i, int j){
+        if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == '0'){
+            return;
+        }
+        grid[i][j] = '0';
+        for(int[] d : direction){
+            dfs(grid, i + d[0], j + d[1]);
+        }
+    }
+}
