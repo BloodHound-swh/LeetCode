db65f794b03d82566c9557d44c507a46ab91fb67
BloodHound-swh <bloodhoundswh@gmail.com>
2018-09-20 09:48:36 +0800

    Two Sum

--- 按题号排序/001-两数之和.java
+++ 按题号排序/001-两数之和.java
@@ -1,14 +1,42 @@
-/*
-两数之和
-给定一个整数数列，找出其中和为特定值的那两个数。
-你可以假设每个输入都只会有一种答案，同样的元素不能被重用。
+/**
+ * 两数之和
+
+给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
+
+你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
+
 示例:
+
 给定 nums = [2, 7, 11, 15], target = 9
+
 因为 nums[0] + nums[1] = 2 + 7 = 9
 所以返回 [0, 1]
  */
 
-//最简单的方法，双重循环
+
+// 未看答案版，由于没有注意到题目中说答案是唯一的，也就是res只是一个2维数组而已，所以错误的使用了list来记录答案
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int temp = target - nums[i];
+            if (map.containsKey(temp)) {
+                list.add(map.get(temp));
+                list.add(i);
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        int[] res = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            res[i] = list.get(i);
+        }
+        return res;
+    }
+}
+
+// 答案一，超时，暴力循环
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
@@ -25,6 +53,7 @@ class Solution {
     }
 }
 
+// 答案二
 // 用哈希表先把数组中的数字和对应的下标存储一遍，即数字作为键，下标作为值，存储.
 // 当遍历数组的时候用target-nums[i]，得到差v，然后在map中找是否存在 v，找到即返回v所对应的value,也就是所对应的数组下标。
 class Solution {
@@ -36,7 +65,7 @@ class Solution {
 
         for (int i = 0; i < nums.length; i++) {
             int v = target - nums[i];
-            if (map.containsKey(v) && i != map.get(v)) {
+            if (map.containsKey(v) && i != map.get(v)) { // 注意第二个条件，防止自己刚好是目标值一半的时候把自己的index写进答案了
                 result[0] = i;
                 result[1] = map.get(v);
                 return result;
@@ -46,6 +75,7 @@ class Solution {
     }
 }
 
+// 答案三，也是不看答案版的标准写法
 // 上面方法的简便写法，遍历数组时，将numbers[i]与target的差值作为键，坐标作为值存储。并判断下一个数组元素值是否是map中的一个键
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
