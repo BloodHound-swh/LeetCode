0ccfd1feb79bab079803c4769950c1c885750204
BloodHound-swh <bloodhoundswh@gmail.com>
2018-09-25 08:31:20 +0800

    First Unique Character in a String

--- 按题号排序/387-字符串中的第一个唯一字符.java
+++ 按题号排序/387-字符串中的第一个唯一字符.java
@@ -1,16 +1,40 @@
-/*
-字符串中的第一个唯一字符
+/**
+ * 字符串中的第一个唯一字符
+
 给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
+
 案例:
+
 s = "leetcode"
 返回 0.
+
 s = "loveleetcode",
 返回 2.
- 
-注意事项：您可以假定该字符串只包含小写字母。
  */
 
-//使用Map的键值对进行判定
+
+// 未看答案版
+class Solution {
+    public int firstUniqChar(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        char[] str = s.toCharArray();
+        for (int i = 0; i < str.length; i++) {
+            if (map.containsKey(str[i])) {
+                map.put(str[i], map.get(str[i]) + 1);
+            } else {
+                map.put(str[i], 1);
+            }
+        }
+        for (int i = 0; i < str.length; i++) {
+            if (map.get(str[i]) == 1)
+                return i;
+        }
+        return -1;
+    }
+}
+
+// 答案一
+// 使用Map的键值对进行判定
 class Solution {
     public int firstUniqChar(String s) {
         char[] arr = s.toCharArray();
