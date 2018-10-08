1a5de58182237134ed21342d8d2d2f66ec554916
BloodHound-swh <bloodhoundswh@gmail.com>
2018-10-08 09:24:21 +0800

    Delete Node in a Linked List

--- 经典题/初级/链表/01-删除链表的节点.java
+++ 经典题/初级/链表/01-删除链表的节点.java
@@ -5,22 +5,17 @@
 比如：假设该链表为 1 -> 2 -> 3 -> 4  ，给定您的为该链表中值为 3 的第三个节点，那么在调用了您的函数之后，该链表则应变成 1 -> 2 -> 4 。
  */
 
-
-
-
-
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
+/*
+ Definition for singly-linked list. 
+ public class ListNode { 
+    int val; 
+    ListNode next; 
+    ListNode(int x) { val = x; } }
  */
- //简单的数据结构，与c不同的是，没有free()，直接把next节点复制到待删除的节点，并重新指定next
+// 简单的数据结构，与c不同的是，没有free()，直接把next节点复制到待删除的节点，并重新指定next
 class Solution {
     public void deleteNode(ListNode node) {
-        if(node == null || node.next == null){
+        if (node == null || node.next == null) {
             return;
         }
         node.val = node.next.val;
