ff7eabb66ff5759ae4b8560cb29aa1aaa0e08639
BloodHound-swh <bloodhoundswh@gmail.com>
2018-10-11 22:00:20 +0800

    对角线遍历

---
+++ 数据结构基础/数组和字符串/06-二维数组简介.java
@@ -0,0 +1,29 @@
+/**
+ * 类似于一维数组，二维数组也是由元素的序列组成。但是这些元素可以排列在矩形网格中而不是直线上。
+ */
+public class Main {
+    private static void printArray(int[][] a) {
+        for (int i = 0; i < a.length; ++i) {
+            System.out.println(a[i]);
+        }
+        for (int i = 0; i < a.length; ++i) {
+            for (int j = 0; a[i] != null && j < a[i].length; ++j) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Example I:");
+        int[][] a = new int[2][5];
+        printArray(a);
+        System.out.println("Example II:");
+        int[][] b = new int[2][];
+        printArray(b);
+        System.out.println("Example III:");
+        b[0] = new int[3];
+        b[1] = new int[5];
+        printArray(b);
+    }
+}
