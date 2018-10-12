e247ff18dc390362660268c246c3fd9dd1cfb662
BloodHound-swh <bloodhoundswh@gmail.com>
2018-10-12 09:08:02 +0800

    Binary Tree Level Order Traversal

--- 按题号排序/102-二叉树的层次遍历.java
+++ 按题号排序/102-二叉树的层次遍历.java
@@ -1,5 +1,6 @@
-/*
-二叉树的层次遍历
+/**
+ * 二叉树的层次遍历
+题目描述提示帮助提交记录社区讨论阅读解答
 给定一个二叉树，返回其按层次遍历的节点值。 （即逐层地，从左到右访问所有节点）。
 
 例如:
@@ -19,17 +20,38 @@
 ]
  */
 
-/*
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
+// 未看答案版，使用队列（注意不是stack，否则输出就反了）来记录每一层的节点值
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
 
-// 使用一个队列queue,按顺序装入每层的节点，queue.size()记录每层节点的个数。然后把每层的节点值add进list中，最后把一个个list放入打最终的list of list
+        Queue<TreeNode> s = new LinkedList<TreeNode>();
+        s.offer(root);
+
+        while (!s.isEmpty()) {
+            List<Integer> l = new ArrayList<>();
+            int size = s.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode temp = s.poll();
+                l.add(temp.val);
+                if (temp.left != null)
+                    s.offer(temp.left);
+                if (temp.right != null)
+                    s.offer(temp.right);
+            }
+            result.add(l);
+        }
+
+        return result;
+    }
+}
+
+// 答案一，同未看答案版的思想
+// 使用一个队列queue,按顺序装入每层的节点，queue.size()记录每层节点的个数。
+// 然后把每层的节点值add进list中，最后把一个个list放入打最终的list of list
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
