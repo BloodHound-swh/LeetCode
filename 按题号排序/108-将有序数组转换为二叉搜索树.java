5b373bd3151e6cf441a358b3eb89c46cdb3e070a
BloodHound-swh <bloodhoundswh@gmail.com>
2018-10-15 08:39:02 +0800

    Convert Sorted Array to Binary Search Tree

--- 按题号排序/108-将有序数组转换为二叉搜索树.java
+++ 按题号排序/108-将有序数组转换为二叉搜索树.java
@@ -1,5 +1,6 @@
-/*
-将有序数组转换为二叉搜索树
+/**
+ * 将有序数组转换为二叉搜索树
+
 将一个按照升序排列的有序数组，转换为一棵高度平衡二叉搜索树。
 
 本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
@@ -17,19 +18,15 @@
  -10  5
  */
 
-/*
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 
+// 未看答案没有写出来，没有意识到这种找根节点的题可以使用二分法
+
+
+
+// 答案
 // 因为是有序数组，所以使用二分法，先把有序数组的最中间值设为根结点，然后根结点的左孩子是左半段数组的最中间值，根结点的右孩子是右半段数组的最中间值。
 // 依次这样，即可以建立一个平衡二叉搜索树。
-// [0,-10,5,null,-3,null,9]
+// 用这个方法做出的答案是[0,-10,5,null,-3,null,9]
 class Solution {
     public TreeNode sortedArrayToBST(int[] nums) {
         if (nums == null)
