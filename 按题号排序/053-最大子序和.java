ada3485f4cd7d403a5f3834b4a9c5cc980d91b19
BloodHound-swh <bloodhoundswh@gmail.com>
2018-10-16 08:57:56 +0800

    Maximum Subarray

--- 按题号排序/053-最大子序和.java
+++ 按题号排序/053-最大子序和.java
@@ -1,4 +1,4 @@
-/*
+/**
  * 最大子序和
 
 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
@@ -13,27 +13,31 @@
 如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
  */
 
-// 可使用动态规划来解决。时间复杂度为O(n)。假设已知0, .., k的最大和sum[k]以后，则0, ..., k+1的最大和sum[k+1]分为以下两种情况： 
-// 1）若sum[k]>=0，则sum[k+1]=sum[k]+A[k+1]。 
+// 未看答案没有做出，未能找出何时更新子串和的方法
+
+// 答案一
+// 可使用动态规划来解决。时间复杂度为O(n)。假设已知0, .., k的和sum[k]以后，则0, ..., k+1的和sum[k+1]分为以下两种情况：
+// 1）若sum[k]>=0，则sum[k+1]=sum[k]+A[k+1]。
 // 2）若sum[k]<0，另起一个SubArray，令sum[k+1]=A[k+1]。
 // 在计算过程中，使用一个变量maxsum用于存储sum的最大值，一旦出现更大的sum值则更新之，最后返回该变量即可。
 class Solution {
     public int maxSubArray(int[] nums) {
         if (nums.length == 0)
             return 0;
-        int maxSubArray = nums[0];
-        int sum = 0;
+        int currSum = 0;
+        int sum = nums[0];
         for (int i = 0; i < nums.length; i++) {
-            sum = sum + nums[i];
-            if (sum > maxSubArray)
-                maxSubArray = sum;
-            if (sum < 0)
-                sum = 0;
+            currSum = currSum + nums[i];
+            if (currSum > sum)
+                sum = currSum;
+            if (currSum < 0)
+                currSum = 0;
         }
-        return maxSubArray;
+        return sum;
     }
 }
 
+// 答案二
 // 使用maxCurrent和max来分别储存按顺序序走的当前子串最大值（之前的子串和小于零则重置），和最终的最大值
 class Solution {
     public int maxSubArray(int[] nums) {
@@ -44,5 +48,23 @@ class Solution {
             max = Math.max(max, maxCurrent);
         }
         return max;
+    }
+}
+
+// 答案三，同答案二
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = Integer.MIN_VALUE;
+        int total_sum = 0;
+        int i = 0;
+        while (i < nums.length) {
+            total_sum += nums[i];
+            sum = Math.max(sum, total_sum);
+            if (total_sum < 0) {
+                total_sum = 0;
+            }
+            i++;
+        }
+        return sum;
     }
 }
