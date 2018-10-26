b723bb65b19d888c1bb372406aca20badfd3e216
BloodHound-swh <bloodhoundswh@gmail.com>
2018-10-26 09:18:50 +0800

    Pascal's Triangle

--- 按题号排序/118-杨辉三角.java
+++ 按题号排序/118-杨辉三角.java
@@ -1,6 +1,6 @@
-/*
- * 
- * 帕斯卡三角形
+/**
+ * 杨辉三角
+
 给定一个非负整数 numRows，生成杨辉三角的前 numRows 行。
 
 
@@ -20,6 +20,43 @@
 ]
  */
 
+// 未看答案版
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if (numRows == 0) {
+            return res;
+        }
+        List<Integer> l1 = new ArrayList<Integer>();
+        l1.add(1);
+        if (numRows == 1) {
+            res.add(l1);
+            return res;
+        }
+        List<Integer> l2 = new ArrayList<Integer>();
+        l2.add(1);
+        l2.add(1);
+        if (numRows == 2) {
+            res.add(l1);
+            res.add(l2);
+            return res;
+        }
+        res.add(l1);
+        res.add(l2);
+        for (int i = 2; i < numRows; i++) {
+            List<Integer> temp = new ArrayList<Integer>();
+            temp.add(1);
+            for (int j = 1; j < i; j++) {
+                temp.add(res.get(i - 1).get(j - 1) + res.get(i - 1).get(j));
+            }
+            temp.add(1);
+            res.add(temp);
+        }
+        return res;
+    }
+}
+
+// 答案一
 // 注意list.add(0,1)是插入制定位置，list.set(0,1)是在指定位置赋值,每次在row的第0位插入1,相当于另起一行了。
 class Solution {
     public List<List<Integer>> generate(int numRows) {
@@ -35,8 +72,8 @@ class Solution {
     }
 }
 
+// 答案二
 // 直观的写法
-// 单纯的list.add(1)表示在第一个位置添加1（在第0位插入1），然后依次往后添加
 class Solution {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> result = new ArrayList<>();
@@ -63,6 +100,7 @@ class Solution {
     }
 }
 
+// 思想同方法二
 public class Solution {
     public ArrayList<ArrayList<Integer>> generate(int numRows) {
         ArrayList<ArrayList<Integer>> rst = new ArrayList<ArrayList<Integer>>();
