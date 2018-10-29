4ad53e5e4f58ae78f592aae4524a878aedf25831
BloodHound-swh <bloodhoundswh@gmail.com>
2018-10-29 08:42:46 +0800

    Valid Parentheses

--- 按题号排序/020-有效的括号.java
+++ 按题号排序/020-有效的括号.java
@@ -1,5 +1,6 @@
-/*
+/**
  * 有效的括号
+
 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
 
 有效字符串需满足：
@@ -30,6 +31,36 @@
 输出: true
  */
 
+// 看了一下提示，使用栈的方法进行匹配弹出
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') {
+                stack.push(s.charAt(i));
+            } else if (s.charAt(i) == ')') {
+                if (stack.isEmpty() || stack.peek() != '(')
+                    return false;
+                stack.pop();
+            } else if (s.charAt(i) == ']') {
+                if (stack.isEmpty() || stack.peek() != '[')
+                    return false;
+                stack.pop();
+            } else if (s.charAt(i) == '}') {
+                if (stack.isEmpty() || stack.peek() != '{')
+                    return false;
+                stack.pop();
+            }
+        }
+        if (stack.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+// 答案一
 // 运用stack,每次出现右括号的时候,pop出stack中最近储存的左括号比较
 class Solution {
     public boolean isValid(String s) {
@@ -56,6 +87,7 @@ class Solution {
     }
 }
 
+// 答案二
 // 换一种思路,主动地在出现左括号时，将匹配的右括号push进stack中，在出现右括号时，与push出来的右括号比较
 class Solution {
     public boolean isValid(String s) {
