60e3678a7e8cfb408c0c0a73c922c6760da0cd13
BloodHound-swh <bloodhoundswh@gmail.com>
2018-11-14 09:03:07 +0800

    Word Search

--- 按题号排序/079-单词搜索.java
+++ 按题号排序/079-单词搜索.java
@@ -1,5 +1,6 @@
-/*
- * 单词搜索
+/**
+ *  单词搜索
+
 给定一个二维网格和一个单词，找出该单词是否存在于网格中。
 
 单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母不允许被重复使用。
@@ -18,6 +19,47 @@ board =
 给定 word = "ABCB", 返回 false.
  */
 
+// 未看答案版（其实不完全没看答案）
+// 使用的是深度优先搜索
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        if (board == null) {
+            return false;
+        }
+        boolean[][] visited = new boolean[board.length][board[0].length];
+        char[] w = word.toCharArray();
+        
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (existHelper(board, w, visited, 0, i, j)) {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+    
+    public boolean existHelper(char[][] board, char[] w, boolean[][] visited, int idx, int i, int j) {
+        if (idx == w.length) 
+            return true;
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length)
+            return false;
+        if (visited[i][j] == true || board[i][j] != w[idx])
+            return false;
+        
+        visited[i][j] = true;
+        boolean flag = existHelper(board, w, visited, idx + 1, i + 1, j)
+                    || existHelper(board, w, visited, idx + 1, i - 1, j)
+                    || existHelper(board, w, visited, idx + 1, i, j + 1)
+                    || existHelper(board, w, visited, idx + 1, i, j - 1);
+        
+        visited[i][j] = false; // 别漏了这一行。
+        return flag;
+    }
+}
+
+// 答案一
 // 类似于岛屿个数那题，使用boolean数组记录字母是否被用过，然后从当前字母的上下左右进行搜索
 class Solution {
     public boolean exist(char[][] board, String word) {
@@ -65,6 +107,7 @@ class Solution {
     }
 }
 
+// 答案二
 // 对上的的方法的代码进行简化，但是由于在方向上没有if的判断，所以运行时间会长一些
 class Solution {
     public boolean exist(char[][] board, String word) {
