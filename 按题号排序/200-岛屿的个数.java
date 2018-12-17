b03000156fc80023213df65f8ac78e73e3422e9c
BloodHound-swh <bloodhoundswh@gmail.com>
2018-12-17 08:42:12 +0800

    Number of Islands

--- 按题号排序/200-岛屿的个数.java
+++ 按题号排序/200-岛屿的个数.java
@@ -1,5 +1,6 @@
-/*
+/**
  * 岛屿的个数
+题目描述提示帮助提交记录社区讨论阅读解答
 给定一个由 '1'（陆地）和 '0'（水）组成的的二维网格，计算岛屿的数量。一个岛被水包围，并且它是通过水平方向或垂直方向上相邻的陆地连接而成的。你可以假设网格的四个边均被水包围。
 
 示例 1:
@@ -20,9 +21,11 @@
 00011
 
 输出: 3
- * 
  */
 
+// 未看答案，想到了使用DFS，但技术细节没有处理好
+
+// 答案一
 // 使用深度优先遍历，探寻每个‘1’的上下左右，如果为1，则置零，如果都不为1，则形成了一个岛屿，res++
 class Solution {
     private int m;
@@ -34,6 +37,7 @@ class Solution {
         if (m == 0)
             return 0;
         n = grid[0].length;
+
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '1') {
@@ -42,20 +46,22 @@ class Solution {
                 }
             }
         }
+
         return res;
     }
 
-    private void dfs(char[][] grid, int i, int j) {
-        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == '0')
+    public void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == '0')
             return;
         grid[i][j] = '0';
-        dfs(grid, i, j + 1);
-        dfs(grid, i, j - 1);
         dfs(grid, i + 1, j);
         dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
     }
 }
 
+// 答案二
 // 对dfs的美化，没什么区别，就是简洁一些
 class Solution {
     private int m, n;
