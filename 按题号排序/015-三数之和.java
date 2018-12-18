16dd6aa891baec78b5bb9b6bfaaa59f4aeed4839
BloodHound-swh <bloodhoundswh@gmail.com>
2018-12-18 08:56:16 +0800

    3Sum

--- 按题号排序/015-三数之和.java
+++ 按题号排序/015-三数之和.java
@@ -1,5 +1,6 @@
-/*
+/**
  * 三数之和
+
 给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？找出所有满足条件且不重复的三元组。
 
 注意：答案中不可以包含重复的三元组。
@@ -13,39 +14,44 @@
 ]
  */
 
+
+// 未看答案，没想到使用双指针，而局限于map之中了
+
+// 答案一
 //使用base指针，和twoSum的方法的左右指针
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        ArrayList<List<Integer>> res = new ArrayList<List<Integer>>();
+        List<List<Integer>> res = new ArrayList<>();
         if (nums == null || nums.length <= 2)
             return res;
-        int n = nums.length;
+        int n = nums.length - 1;
         int i = 0;
         Arrays.sort(nums);
-        while (i < n - 2) {
+        while (i <= n - 2) {
             int base = nums[i];
             int left = i + 1;
-            int right = n - 1;
+            int right = n;
 
             while (left < right) {
                 int sum = base + nums[left] + nums[right];
                 if (sum == 0) {
-                    LinkedList<Integer> list = new LinkedList<Integer>();
+                    ArrayList<Integer> list = new ArrayList<>();
                     list.add(base);
                     list.add(nums[left]);
                     list.add(nums[right]);
                     res.add(list);
                     left = moveRight(nums, left + 1);
                     right = moveLeft(nums, right - 1);
-                } else if (sum > 0) {
-                    right = moveLeft(nums, right - 1);
-                } else {
+                } else if (sum < 0) {
                     left = moveRight(nums, left + 1);
+                } else {
+                    right = moveLeft(nums, right - 1);
                 }
             }
 
             i = moveRight(nums, i + 1);
         }
+
         return res;
     }
 
@@ -64,28 +70,25 @@ class Solution {
     }
 }
 
-// 代码优化
+// 答案二
+//代码优化
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
-            int low = i + 1, high = nums.length - 1, sum = 0 - nums[i];
-            while (low < high) {
-                if (nums[low] + nums[high] == sum) {
+        for(int i = 0;i < nums.length - 2; i++){
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+            int low = i + 1, high = nums.length -1, sum = 0 - nums[i];
+            while(low < high){
+                if(nums[low] + nums[high] == sum){
                     res.add(Arrays.asList(nums[i], nums[low], nums[high]));
-                    while (low < high && nums[low] == nums[low + 1])
-                        low++;
-                    while (low < high && nums[high] == nums[high - 1])
-                        high--;
+                    while(low < high && nums[low] == nums[low + 1]) low ++;
+                    while(low < high && nums[high] == nums[high - 1]) high --;
                     low++;
                     high--;
-                } else if (nums[low] + nums[high] < sum) {
-                    low++;
-                } else
-                    high--;
+                } else if (nums[low] + nums[high] < sum){
+                    low ++;
+                } else high --;
             }
         }
         return res;
