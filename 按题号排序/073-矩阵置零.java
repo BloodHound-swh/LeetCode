3ecb4e4f4e5c01dcb9ab675b08d7d687afa9adee
BloodHound-swh <bloodhoundswh@gmail.com>
2018-12-19 09:15:53 +0800

    Set Matrix Zeroes

--- 按题号排序/073-矩阵置零.java
+++ 按题号排序/073-矩阵置零.java
@@ -1,5 +1,6 @@
-/*
+/**
  * 矩阵置零
+
 给定一个 m x n 的矩阵，如果一个元素为 0，则将其所在行和列的所有元素都设为 0。请使用原地算法。
 
 示例 1:
@@ -37,7 +38,51 @@
 你能想出一个常数空间的解决方案吗？
  */
 
-//将第一行与第一列作为标志位，标志位为零，则整下的那一行或那一列置零，并用两个boolean变量判定第一列与第一行本身的变化
+// 未看答案没有做出
+
+// 答案一
+// 我们只需要直到哪些行，哪些列需要被置0就行了，最简单的方法就是建两个大小分别为M和N的数组，来记录哪些行哪些列应该被置0。
+// 那有没有可能不用额外空间呢？我们其实可以借用原矩阵的首行和首列来存储这个信息。
+// 这个方法的缺点在于，如果我们直接将0存入首行或首列来表示相应行和列要置0的话，我们很难判断首行或者首列自己是不是该置0。
+// 这里我们用两个boolean变量记录下首行和首列原本有没有0，然后在其他位置置完0后，再单独根据boolean变量来处理首行和首列，就避免了干扰的问题。
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        if (matrix.length == 0)
+            return;
+        boolean firstRowZero = false;
+        boolean firstColZero = false;
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (i != 0 && j != 0 && matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                } else if (matrix[i][j] == 0) {
+                    firstRowZero = i == 0 ? true : firstRowZero;
+                    firstColZero = j == 0 ? true : firstColZero;
+                }
+            }
+        }
+
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 0; firstColZero && i < matrix.length; i++) {
+            matrix[i][0] = 0;
+        }
+
+        for (int j = 0; firstRowZero && j < matrix[0].length; j++) {
+            matrix[0][j] = 0;
+        }
+    }
+}
+
+// 答案二，另一种写法
 class Solution {
     public void setZeroes(int[][] matrix) {
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
