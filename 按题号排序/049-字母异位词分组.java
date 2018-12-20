bd21bdd3e64b5f31bdcba4ce1eb35a60f8a53304
BloodHound-swh <bloodhoundswh@gmail.com>
2018-12-20 08:56:59 +0800

    Group Anagrams

--- 按题号排序/049-字母异位词分组.java
+++ 按题号排序/049-字母异位词分组.java
@@ -1,5 +1,6 @@
-/*
- * 字谜分组
+/**
+ * 字母异位词分组
+
 给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
 
 示例:
@@ -17,13 +18,15 @@
 不考虑答案输出的顺序。
  */
 
+// 未看答案没有做出，无思路。。。
 
-
+// 答案一
 //使用Hashmap来存放字母异位词，将词组中每个单词的字母排序，作为key，相同的key则将该单词加入进对应的value的list中，否则新建一个key-value(list)
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         ArrayList<List<String>> res = new ArrayList<List<String>>();
-        if (strs == null || strs.length == 0) return res;
+        if (strs == null || strs.length == 0)
+            return res;
         HashMap<String, List<String>> map = new HashMap<String, List<String>>();
         for (int i = 0; i < strs.length; i++) {
             char[] curr = strs[i].toCharArray();
@@ -33,15 +36,15 @@ class Solution {
                 map.put(key, new ArrayList<String>());
             map.get(key).add(strs[i]);
         }
-        
+
         return new ArrayList<List<String>>(map.values());
         // res.addAll(map.values()); 也可以
         // return res;
     }
 }
 
-
-//优化 map.putIfAbsent 等效于
+// 同答案一
+//注意 map.putIfAbsent 等效于
 // if (!map.containsKey(key)) 
 //     return map.put(key, value);
 // else
@@ -58,5 +61,29 @@ public class Solution {
             map.get(key).add(s);
         }
         return new ArrayList<>(map.values());
+    }
+}
+
+// 答案二，计数分类
+// https://leetcode-cn.com/articles/group-anagrams/
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs.length == 0) return new ArrayList();
+        Map<String, List> ans = new HashMap<String, List>();
+        int[] count = new int[26];
+        for (String s : strs) {
+            Arrays.fill(count, 0);
+            for (char c : s.toCharArray()) count[c - 'a']++;
+
+            StringBuilder sb = new StringBuilder("");
+            for (int i = 0; i < 26; i++) {
+                sb.append('#');
+                sb.append(count[i]);
+            }
+            String key = sb.toString();
+            if (!ans.containsKey(key)) ans.put(key, new ArrayList());
+            ans.get(key).add(s);
+        }
+        return new ArrayList(ans.values());
     }
 }
