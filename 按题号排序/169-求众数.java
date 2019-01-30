318e636db32b76eb36e64e2dcfb34d0054afb4af
BloodHound-swh <bloodhoundswh@gmail.com>
2019-01-30 22:02:56 +0800

    Majority Element

--- 按题号排序/169-求众数.java
+++ 按题号排序/169-求众数.java
@@ -1,5 +1,6 @@
-/*
+/**
  * 求众数
+
 给定一个大小为 n 的数组，找到其中的众数。众数是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
 
 你可以假设数组是非空的，并且给定的数组总是存在众数。
@@ -14,6 +15,28 @@
 输出: 2
  */
 
+
+// 未看答案版，很传统也很简单，使用HashMap
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int res = 0;
+        for (int num : nums) {
+            if (!map.containsKey(num)) {
+                map.put(num, 1);
+            } else {
+                map.put(num, map.get(num) + 1);
+            }
+            if (map.get(num) > nums.length / 2) {
+                res = num;
+                break;
+            }
+        }
+        return res;
+    }
+}
+
+// 答案一
 // 传统思路，使用HashMap来记录数字与出现次数
 class Solution {
     public int majorityElement(int[] nums) {
@@ -34,6 +57,7 @@ class Solution {
     }
 }
 
+// 答案二
 // 从第一个数开始，使用count来记录次数，出现相同数字count++，不同时count--，当count为0时，就res制定为下一个数
 // 原理：由于众数个数>n/2，且题目限制众数一定存在，所以只要找到不同的两个数就消掉，最后剩下的一定就是众数
 class Solution {
@@ -55,7 +79,7 @@ class Solution {
     }
 }
 
-// 同方法2
+// 同答案二
 class Solution {
     public int majorityElement(int[] nums) {
         int main = nums[0]; // 用于记录主元素，假设第一个是主元素
@@ -76,6 +100,7 @@ class Solution {
     }
 }
 
+// 答案三
 // 取巧，直接将数组排序，取中间的那一个，肯定是众数
 class Solution {
     public int majorityElement(int[] nums) {
