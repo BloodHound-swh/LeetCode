006ea72a46c64acb4eae0f146f628aa412709b9c
BloodHound-swh <bloodhoundswh@gmail.com>
2019-02-11 21:49:27 +0800

    Task Scheduler

--- 按题号排序/621-任务调度器.java
+++ 按题号排序/621-任务调度器.java
@@ -1,5 +1,6 @@
-/*
- * Task Scheduler
+/**
+ * 任务调度器
+
 给定一个用字符数组表示的 CPU 需要执行的任务列表。其中包含使用大写的 A - Z 字母表示的26 种不同种类的任务。任务可以以任意顺序执行，并且每个任务都可以在 1 个单位时间内执行完。CPU 在任何一个单位时间内都可以执行一个任务，或者在待命状态。
 
 然而，两个相同种类的任务之间必须有长度为 n 的冷却时间，因此至少有连续 n 个单位时间内 CPU 在执行不同的任务，或者在待命状态。
@@ -17,9 +18,12 @@
 n 的取值范围为 [0, 100]。
  */
 
+// 未看答案，大概能猜到是插入法（实际上是贪心算法），但是具体怎么写不会
 
 
-// 三种方法都是用的一个思路，只是写法不同而已
+// 三种答案都是用的一个思路，只是写法不同而已
+
+// 答案一
 // 按照出现频率最多（假设频率为k）的将其分为了k块，然后每一轮向这k个区间个插入其他任务，如果不够n则加入idle凑齐n，这样 每个区间就是n + 1f
 // 若是最高频率元素为s个，那么最后就加上s，因为这s个频率相同的任务经过前面的分配都只剩下一个了。
 // 若是最高频率都用完了，但是还是没有把所有task执行完，那么总时间就是task的长度（我也不知道怎么证明）
@@ -29,16 +33,19 @@ class Solution {
         for (char task : tasks) {
             freq[task - 'A']++;
         }
-        int maxFreq = 0, maxFreqCount = 0;
+        int maxFreq = 0;
+        int maxFreqTask = 0;
+
         for (int i = 0; i < 26; i++) {
             if (freq[i] > maxFreq) {
                 maxFreq = freq[i];
-                maxFreqCount = 1;
+                maxFreqTask = 1;
             } else if (freq[i] == maxFreq) {
-                maxFreqCount++;
+                maxFreqTask++;
             }
         }
-        return Math.max(tasks.length, (maxFreq - 1) * (n + 1) + maxFreqCount);
+
+        return Math.max(tasks.length, (maxFreq - 1) * (n + 1) + maxFreqTask);
     }
 }
 
