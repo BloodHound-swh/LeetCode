1edbbdcc25fc786f5415716d07c4e36de023dfb4
BloodHound-swh <bloodhoundswh@gmail.com>
2019-02-28 09:19:57 +0800

    数组中两个数的最大异或值

---
+++ 数据结构基础/前缀树/04-数组中两个数的最大异或值.java
@@ -0,0 +1,38 @@
+/**
+ * 数组中两个数的最大异或值
+给定一个非空数组，数组中元素为 a0, a1, a2, … , an-1，其中 0 ≤ ai < 231 。
+
+找到 ai 和aj 最大的异或 (XOR) 运算结果，其中0 ≤ i,  j < n 。
+
+你能在O(n)的时间解决这个问题吗？
+
+示例:
+
+输入: [3, 10, 5, 25, 2, 8]
+
+输出: 28
+
+解释: 最大的结果是 5 ^ 25 = 28.
+ */
+
+// 没看懂。。。
+class Solution {
+    public int findMaximumXOR(int[] nums) {
+        int max = 0, mask = 0;
+        for (int i = 31; i >= 0; i--) {
+            mask = mask | (1 << i);
+            Set<Integer> set = new HashSet<>();
+            for (int num : nums) {
+                set.add(num & mask);
+            }
+            int tmp = max | (1 << i);
+            for (int prefix : set) {
+                if (set.contains(tmp ^ prefix)) {
+                    max = tmp;
+                    break;
+                }
+            }
+        }
+        return max;
+    }
+}
