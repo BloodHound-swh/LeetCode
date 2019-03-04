16c0bdd5cf6e30534a7fa4c3779675444fe2ef26
BloodHound-swh <bloodhoundswh@gmail.com>
2019-03-04 08:43:21 +0800

    Implement Trie (Prefix Tree)

--- 按题号排序/208-实现 Trie (前缀树).java
+++ 按题号排序/208-实现 Trie (前缀树).java
@@ -1,6 +1,6 @@
-/*
+/**
  * 实现 Trie (前缀树)
-题目描述提示帮助提交记录社区讨论阅读解答
+
 实现一个 Trie (前缀树)，包含 insert, search, 和 startsWith 这三个操作。
 
 示例:
@@ -19,6 +19,67 @@ trie.search("app");     // 返回 true
 保证所有输入均为非空字符串。
  */
 
+// 未看答案版，发现在search和startwith方法其实可以公用一些代码的
+class Trie {
+    class TrieNode {
+        public boolean is_word;
+        public TrieNode[] children;
+
+        TrieNode() {
+            is_word = false;
+            children = new TrieNode[26];
+        }
+    }
+
+    /** Initialize your data structure here. */
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    TrieNode root;
+
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        TrieNode p = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (p.children[c - 'a'] == null)
+                p.children[c - 'a'] = new TrieNode();
+            p = p.children[c - 'a'];
+        }
+        p.is_word = true;
+    }
+
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        TrieNode p = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (p.children[c - 'a'] != null)
+                p = p.children[c - 'a'];
+            else
+                return false;
+        }
+        return p.is_word;
+    }
+
+    /**
+     * Returns if there is any word in the trie that starts with the given prefix.
+     */
+    public boolean startsWith(String prefix) {
+        TrieNode p = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            if (p.children[c - 'a'] != null)
+                p = p.children[c - 'a'];
+            else
+                return false;
+        }
+        return true;
+    }
+}
+
+// 答案一
 // https://www.youtube.com/watch?v=f48wGD-MuQw
 // 初始化一个dummy的root节点，它下面相当于有26个子树，初始都为空
 // 插入单词的时候，遍历单词中的字母，一级级往下走，对每个字母对应位置的子树进行Trienode构造
@@ -78,12 +139,6 @@ class Trie {
         return p;
     }
 }
-
-/**
- * Your Trie object will be instantiated and called as such: Trie obj = new
- * Trie(); obj.insert(word); boolean param_2 = obj.search(word); boolean param_3
- * = obj.startsWith(prefix);
- */
 
 // 使用HashMap，思想同方法一
 class Trie {
