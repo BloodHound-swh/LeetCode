6515ade619381c41ceb4bfeace90f7310ad9ab6b
BloodHound-swh <bloodhoundswh@gmail.com>
2019-03-08 08:34:18 +0800

    Largest Number

--- 按题号排序/179-最大数.java
+++ 按题号排序/179-最大数.java
@@ -1,5 +1,6 @@
 /**
- *  最大数
+ * 最大数
+
 给定一组非负整数，重新排列它们的顺序使之组成一个最大的整数。
 
 示例 1:
@@ -14,6 +15,10 @@
  */
 
 
+// 关键在于如何新建Comparator来重新定义排序规则
+
+
+// 答案
 // 从第一位向后比较，如果有一位更大，则该数更大，如9大于15，24大于23。
 // 如果某个数的前半部分和另一个数完全相同，则看该数剩下的那部分和另一个数的大小关系，如2332和23比较时，2332是大于23的，因为32大于23。
 // 不过如果细分各种情况，会弄得非常复杂，这里有个技巧，就是我们把两个数拼在一起，然后再把这两个数换个顺序再拼在一起，这时候就可以直接比较了。
@@ -47,36 +52,6 @@ class Solution {
             sb.append(str[i]);
         }
 
-        return sb.toString();
-    }
-}
-
-
-// 优化（其实只能算是美化）
-class Solution {
-
-    class MyComparator implements Comparator<String> {
-        public int compare(String s1, String s2) {
-            String temp1 = s1 + s2;
-            String temp2 = s2 + s1;
-            int differ = temp2.compareTo(temp1);
-            return differ;
-        }
-    }
-
-    public String largestNumber(int[] nums) {
-        String[] strs = new String[nums.length];
-        for (int i = 0; i < strs.length; i++) {
-            strs[i] = "" + nums[i];
-        }
-        Arrays.sort(strs, new MyComparator());
-        if (strs[0].equals("0")) {
-            return "0";
-        }
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < strs.length; i++) {
-            sb.append(strs[i]);
-        }
         return sb.toString();
     }
 }
