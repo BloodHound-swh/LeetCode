2c2d2e9fee4a98fe8110074a6713c48e8cd2397b
BloodHound-swh <bloodhoundswh@gmail.com>
2019-03-11 08:50:28 +0800

    Queue Reconstruction by Height

--- 按题号排序/406-根据身高重建队列.java
+++ 按题号排序/406-根据身高重建队列.java
@@ -1,5 +1,6 @@
-/*
- * Queue Reconstruction by Height
+/**
+ * 根据身高重建队列
+
 假设有打乱顺序的一群人站成一个队列。 每个人由一个整数对(h, k)表示，其中h是这个人的身高，k是排在这个人前面且身高大于或等于h的人数。 编写一个算法来重建这个队列。
 
 注意：
@@ -14,32 +15,30 @@
 [[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
  */
 
-// 首先我们给队列先排个序，按照身高高的排前面，如果身高相同，则第二个数小的排前面。
-// 然后我们新建一个空的数组，遍历之前排好序的数组，然后根据每个元素的第二个数字，将其插入到res数组中对应的位置
-// 这是因为我们先排了身高大的人，之后排身高小的人是不影响身高大的人的k值的
+// 无思路
+
+
 class Solution {
     public int[][] reconstructQueue(int[][] people) {
-        if (people == null || people.length == 0 || people[0].length == 0) {
+        if (people == null || people.length == 0 || people[0].length == 0)
             return people;
-        }
 
         Arrays.sort(people, new Comparator<int[]>() {
-            public int compare(int[] o1, int[] o2) {
-                if (o1[0] != o2[0]) {
-                    return o2[0] - o1[0];
-                } else {
-                    return o1[1] - o2[1];
-                }
+            public int compare(int[] a, int[] b) {
+                if (a[0] != b[0])
+                    return b[0] - a[0];
+                else
+                    return a[1] - b[1];
             }
         });
 
-        List<int[]> list = new ArrayList<>();
+        List<int[]> list = new LinkedList<>();
         for (int i = 0; i < people.length; i++) {
-            list.add(people[i][1], people[i]); // java.util.ArrayList.add(int index, E elemen) 方法将指定的元素E在此列表中的指定位置。
+            list.add(people[i][1], people[i]);
         }
 
         int[][] res = new int[people.length][people[0].length];
-        for (int i = 0; i < res.length; i++) {
+        for (int i = 0; i < people.length; i++) {
             res[i][0] = list.get(i)[0];
             res[i][1] = list.get(i)[1];
         }
@@ -48,7 +47,7 @@ class Solution {
     }
 }
 
-// 优化，同方法一
+// 或者用toArray
 class Solution {
     public int[][] reconstructQueue(int[][] people) {
         if (people == null || people.length == 0 || people[0].length == 0) {
