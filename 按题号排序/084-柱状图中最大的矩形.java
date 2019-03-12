d00d55052bc860d986c189adf1afe9b151f526be
BloodHound-swh <bloodhoundswh@gmail.com>
2019-03-12 08:30:19 +0800

    Largest Rectangle in Histogram

--- 按题号排序/084-柱状图中最大的矩形.java
+++ 按题号排序/084-柱状图中最大的矩形.java
@@ -1,12 +1,17 @@
-/*
+/**
  * 柱状图中最大的矩形
+
 给定 n 个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。
 
 求在该柱状图中，能够勾勒出来的矩形的最大面积。
 
+ 
+
 
 
 以上是柱状图的示例，其中每个柱子的宽度为 1，给定的高度为 [2,1,5,6,2,3]。
+
+ 
 
 
 
@@ -20,6 +25,9 @@
 输出: 10
  */
 
+// 想不出来。。。
+
+
 // http://www.cnblogs.com/lichen782/p/leetcode_Largest_Rectangle_in_Histogram.html
 // 使用一个栈来维护递增的圆柱所对应的索引
 // 当第i个圆柱体小于栈顶的圆柱体时，开始向左计算面积，直到栈顶的圆柱体再次小于当前圆柱体
@@ -27,30 +35,10 @@ class Solution {
     public int largestRectangleArea(int[] heights) {
         if (heights == null || heights.length == 0)
             return 0;
-        Stack<Integer> stack = new Stack<Integer>();
-        int i = 0;
-        int maxArea = 0;
-        int[] h = new int[heights.length + 1];
-        h = Arrays.copyOf(heights, heights.length + 1);
-        while (i < h.length) {
-            if (stack.isEmpty() || h[stack.peek()] <= h[i]) {
-                stack.push(i++);
-            } else {
-                int t = stack.pop();
-                maxArea = Math.max(maxArea, h[t] * (stack.isEmpty() ? i : i - stack.peek() - 1));
-            }
-        }
-        return maxArea;
-    }
-}
 
-// 这是对上面算法的优化，或者说简洁的写法
-class Solution {
-    public int largestRectangleArea(int[] heights) {
-        if (heights == null || heights.length == 0)
-            return 0;
         Stack<Integer> stack = new Stack<>();
         int res = 0;
+
         for (int i = 0; i <= heights.length; i++) {
             int h = i == heights.length ? 0 : heights[i];
             while (!stack.isEmpty() && h < heights[stack.peek()]) {
