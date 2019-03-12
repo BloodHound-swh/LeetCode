5fc5c6d28fc1043a24ed8ce30f3b214d0becec9f
BloodHound-swh <bloodhoundswh@gmail.com>
2019-03-12 08:57:12 +0800

    LRU Cache

--- 按题号排序/146-LRU缓存机制.java
+++ 按题号排序/146-LRU缓存机制.java
@@ -1,5 +1,6 @@
 /**
- *  LRU 缓存机制
+ * LRU缓存机制
+
 运用你所掌握的数据结构，设计和实现一个  LRU (最近最少使用) 缓存机制。它应该支持以下操作： 获取数据 get 和 写入数据 put 。
 
 获取数据 get(key) - 如果密钥 (key) 存在于缓存中，则获取密钥的值（总是正数），否则返回 -1。
@@ -11,7 +12,7 @@
 
 示例:
 
-LRUCache cache = new LRUCache( 2 / 缓存容量 / );
+LRUCache cache = new LRUCache( 2  缓存容量  );
 
 cache.put(1, 1);
 cache.put(2, 2);
@@ -24,6 +25,91 @@ cache.get(3);       // 返回  3
 cache.get(4);       // 返回  4
  */
 
+
+// 首先O(1)时间取出肯定需要用到HashMap，其次有将最近用过的放在最前面，没用过的放在最后面，并且还要增删，显然要用到链表
+// 这里最好用的是双向链表
+
+class LRUCache {
+
+    int size;
+    int capacity;
+    ListNode head;
+    ListNode tail;
+    Map<Integer, ListNode> map;
+
+    public class ListNode {
+        ListNode prev;
+        ListNode next;
+        int key;
+        int val;
+
+        public ListNode(int k, int v) {
+            this.key = k;
+            this.val = v;
+            this.prev = null;
+            this.next = null;
+        }
+    }
+
+    public LRUCache(int capacity) {
+        this.head = new ListNode(-1, -1);
+        this.tail = new ListNode(-1, -1);
+        head.next = tail;
+        tail.prev = head;
+        this.size = 0;
+        this.capacity = capacity;
+        this.map = new HashMap<Integer, ListNode>();
+    }
+
+    public int get(int key) {
+        ListNode node = map.get(key);
+        if (node != null) {
+            moveToHead(node);
+            return node.val;
+        } else {
+            return -1;
+        }
+    }
+
+    public void put(int key, int value) {
+        ListNode node = map.get(key);
+        if (node == null) {
+            node = new ListNode(key, value);
+            attachToHead(node);
+            size++;
+        } else {
+            node.val = value;
+            moveToHead(node);
+        }
+
+        if (size > capacity) {
+            removeLast();
+            size--;
+        }
+
+        map.put(key, node);
+    }
+
+    private void attachToHead(ListNode node) {
+        node.next = head.next;
+        node.next.prev = node;
+        head.next = node;
+        node.prev = head;
+    }
+
+    private void moveToHead(ListNode node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+        attachToHead(node);
+    }
+
+    private void removeLast() {
+        ListNode last = tail.prev;
+        last.prev.next = tail;
+        tail.prev = last.prev;
+        map.remove(last.key);
+    }
+}
 
 // O(1)的获取速度，这样看来只有哈希表可以胜任，但是哈希表无序的，所以还需要用上队列
 // 所以我们将元素存在队列中，并用一个哈希表记录下键值和元素的映射，就可以做到O(1)获取速度，和先进先出的效果
@@ -114,10 +200,3 @@ class LRUCache {
         map.remove(last.key); // 注意map通过键来去除键值对关系
     }
 }
-
-/**
-* Your LRUCache object will be instantiated and called as such:
-* LRUCache obj = new LRUCache(capacity);
-* int param_1 = obj.get(key);
-* obj.put(key,value);
-*/
