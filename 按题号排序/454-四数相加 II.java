cf852a1d8b6ade0b90a4390dc508cb1a62cf01f2
BloodHound-swh <bloodhoundswh@gmail.com>
2019-03-19 21:41:00 +0800

    4Sum II

--- 按题号排序/454-四数相加 II.java
+++ 按题号排序/454-四数相加 II.java
@@ -1,5 +1,6 @@
-/*
- *  四数相加 II
+/**
+ * 四数相加 II
+
 给定四个包含整数的数组列表 A , B , C , D ,计算有多少个元组 (i, j, k, l) ，使得 A[i] + B[j] + C[k] + D[l] = 0。
 
 为了使问题简单化，所有的 A, B, C, D 具有相同的长度 N，且 0 ≤ N ≤ 500 。所有整数的范围在 -228 到 228 - 1 之间，最终结果不会超过 231 - 1 。
@@ -22,6 +23,7 @@ D = [ 0, 2]
  */
 
 
+
 // 我们如果把A和B的两两之和都求出来，在哈希表中建立两数之和跟其出现次数之间的映射
 // 那么我们再遍历C和D中任意两个数之和，我们只要看哈希表存不存在这两数之和的相反数就行了
 class Solution {
@@ -30,14 +32,15 @@ class Solution {
         for (int i = 0; i < C.length; i++) {
             for (int j = 0; j < D.length; j++) {
                 int sum = C[i] + D[j];
-                map.put(sum, map.getOrDefault(sum, 0) + 1); // 如果key对应的键有值，那么返回这个值；否则返回这个指定的默认值defaultValue
+                map.put(sum, map.containsKey(sum) ? map.get(sum) + 1 : 1);
             }
         }
 
         int res = 0;
         for (int i = 0; i < A.length; i++) {
             for (int j = 0; j < B.length; j++) {
-                res += map.getOrDefault(-1 * (A[i] + B[j]), 0);
+                int sum = A[i] + B[j];
+                res += map.containsKey(-sum) ? map.get(-sum) : 0;
             }
         }
 
