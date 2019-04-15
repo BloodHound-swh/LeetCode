04bc01fa915460ad59f010f9242716c7dbe4eed1
shenweihan <bloodhoundswh@gmail.com>
2019-04-15 21:13:09 +0800

    Climbing Stairs

--- 按题号排序/070-爬楼梯.java
+++ 按题号排序/070-爬楼梯.java
@@ -1,3 +1,29 @@
+/**
+ * 爬楼梯
+假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+
+每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+
+注意：给定 n 是一个正整数。
+
+示例 1：
+
+输入： 2
+输出： 2
+解释： 有两种方法可以爬到楼顶。
+1.  1 阶 + 1 阶
+2.  2 阶
+示例 2：
+
+输入： 3
+输出： 3
+解释： 有三种方法可以爬到楼顶。
+1.  1 阶 + 1 阶 + 1 阶
+2.  1 阶 + 2 阶
+3.  2 阶 + 1 阶
+ */
+
+
 // 未看答案版，有错误，递归的公式不对
 class Solution {
     public int climbStairs(int n) {
@@ -46,5 +72,22 @@ class Solution {
             pre = current - pre;
         }
         return current;
+    }
+}
+
+// 其实就是菲波那切数列
+class Solution {
+    public int climbStairs(int n) {
+        if (n <= 1)
+            return 1;
+        int first = 1;
+        int second = 2;
+        for (int i = 3; i <= n; i++) {
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+
+        return second;
     }
 }
