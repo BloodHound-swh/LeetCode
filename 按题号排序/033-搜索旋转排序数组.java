3b73914d11c74cee49bd649921f745c7489fa343
shenweihan <bloodhoundswh@gmail.com>
2019-04-17 09:28:19 +0800

    Search in Rotated Sorted Array

--- 按题号排序/033-搜索旋转排序数组.java
+++ 按题号排序/033-搜索旋转排序数组.java
@@ -120,3 +120,34 @@ class Solution {
         return -1;
     }
 }
+
+// 第二次复习，使用模板一，且不用helper函数
+class Solution {
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0)
+            return -1;
+        
+        int n = nums.length;
+        int left = 0;
+        int right = n - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } if (nums[left] <= nums[mid]) {
+                if (target < nums[mid] && nums[left] <= target)
+                    right = mid - 1;
+                else 
+                    left = mid + 1;
+            } else if (nums[mid] <= nums[right]) {
+                if (target > nums[mid] && nums[right] >= target) 
+                    left = mid + 1;
+                else 
+                    right = mid - 1;
+            }
+        }
+        
+        return -1;
+    }
+}
