8b142f9284a098ccb1fc90d834c558b46161bf26
shenweihan <bloodhoundswh@gmail.com>
2019-04-17 21:55:15 +0800

    Maximum Depth of Binary Tree

--- 按题号排序/104-二叉树的最大深度.java
+++ 按题号排序/104-二叉树的最大深度.java
@@ -60,3 +60,22 @@ class Solution {
         return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
 }
+
+// 第二次复习
+class Solution {
+    public int maxDepth(TreeNode root) {
+        int maxDepth = dfs(root);
+        
+        return maxDepth;
+        
+    }
+    
+    public int dfs(TreeNode root) {
+        if (root == null) 
+            return 0;
+        int left = dfs(root.left) + 1;
+        int right = dfs(root.right) + 1;
+        
+        return Math.max(left, right);
+    }
+}
