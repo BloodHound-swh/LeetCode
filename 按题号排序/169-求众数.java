6618dd268d3536890cea951ea167162f8fd58290
shenweihan <bloodhoundswh@gmail.com>
2019-04-18 21:18:44 +0800

    Majority Element

--- 按题号排序/169-求众数.java
+++ 按题号排序/169-求众数.java
@@ -108,3 +108,26 @@ class Solution {
         return nums[nums.length / 2];
     }
 }
+
+
+// 第二次复习
+class Solution {
+    public int majorityElement(int[] nums) {
+        int res = nums[0];
+        int count = 1;
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == res) {
+                count++;
+            } else {
+                count--;
+                if (count == 0) {
+                    res = nums[i];
+                    count = 1;
+                }
+            }
+        }
+        
+        return res;
+    }
+}
