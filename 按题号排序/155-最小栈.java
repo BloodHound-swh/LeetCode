e920f1ef559dc6fe018efb971ac7c881d34af1ca
shenweihan <bloodhoundswh@gmail.com>
2019-04-19 08:42:19 +0800

    Min Stack

--- 按题号排序/155-最小栈.java
+++ 按题号排序/155-最小栈.java
@@ -92,3 +92,35 @@ class MinStack {
         return s2.peek();
     }
 }
+
+// 第二次复习
+class MinStack {
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new Stack<Integer>();
+        minStack = new Stack<Integer>();
+    }
+    
+    public void push(int x) {
+        stack.push(x);
+        if (minStack.size() == 0 || x <= minStack.peek())
+            minStack.push(x);
+    }
+    
+    public void pop() {
+        int n = stack.pop();
+        if (minStack.peek() == n)
+            minStack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
