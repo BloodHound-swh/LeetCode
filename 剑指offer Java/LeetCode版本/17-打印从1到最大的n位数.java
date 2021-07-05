38eb5b8c28e8bd64947259e0baf0f68a7b90138d
BloodHound-swh <bloodhoundswh@gmail.com>
2021-07-05 10:12:50 +0800

    20210705

---
+++ 剑指offer Java/LeetCode版本/17-打印从1到最大的n位数.java
@@ -0,0 +1,37 @@
+/**
+ * 剑指 Offer 17. 打印从1到最大的n位数
+输入数字 n，按顺序打印出从 1 到最大的 n 位十进制数。比如输入 3，则打印出 1、2、3 一直到最大的 3 位数 999。
+
+示例 1:
+
+输入: n = 1
+输出: [1,2,3,4,5,6,7,8,9]
+ 
+
+说明：
+
+用返回一个整数列表来代替打印
+n 为正整数
+ */
+
+class Solution {
+    public int[] printNumbers(int n) {
+        int end = (int) Math.pow(10, n);
+        int[] nums = new int[end - 1];
+        for (int i = 1; i <= end - 1; i++) {
+            nums[i - 1] = i;
+        }
+
+        return nums;
+    }
+}
+
+class Solution {
+    public int[] printNumbers(int n) {
+        int end = (int)Math.pow(10, n) - 1;
+        int[] res = new int[end];
+        for(int i = 0; i < end; i++)
+            res[i] = i + 1;
+        return res;
+    }
+}
