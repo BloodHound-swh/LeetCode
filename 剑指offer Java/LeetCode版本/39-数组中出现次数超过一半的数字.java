99d0f9e394cc07e26bec5ff389643d653f5bf064
BloodHound-swh <bloodhoundswh@gmail.com>
2021-08-05 10:02:11 +0800

    20210805

---
+++ 剑指offer Java/LeetCode版本/39-数组中出现次数超过一半的数字.java
@@ -0,0 +1,65 @@
+/**
+ * 剑指 Offer 39. 数组中出现次数超过一半的数字
+数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。
+
+ 
+
+你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+
+ 
+
+示例 1:
+
+输入: [1, 2, 3, 2, 2, 2, 5, 4, 2]
+输出: 2
+ 
+
+限制：
+
+1 <= 数组长度 <= 50000
+
+
+ */
+
+
+// 从第一个数开始，使用count来记录次数，出现相同数字count++，不同时count--，当count为0时，就res制定为下一个数
+// 原理：由于众数个数>n/2，且题目限制众数一定存在，所以只要找到不同的两个数就消掉，最后剩下的一定就是众数
+class Solution {
+    public int majorityElement(int[] nums) {
+        int res = nums[0];
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (res == nums[i]) {
+                count++;
+            } else {
+                count--;
+                if (count == 0) {
+                    res = nums[i];
+                    count = 1;
+                }
+            }
+        }
+
+        return res;
+    }
+}
+
+// 传统思路，使用HashMap来记录数字与出现次数
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int res = 0;
+        for (int num : nums) {
+            if (!map.containsKey(num)) {
+                map.put(num, 1);
+            } else {
+                map.put(num, map.get(num) + 1);
+            }
+            if (map.get(num) > nums.length / 2) {
+                res = num;
+                break;
+            }
+        }
+        return res;
+    }
+}
